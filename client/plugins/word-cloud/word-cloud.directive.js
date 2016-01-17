aff542d8bf005f414bc989e51003ded7fabed592
Andrew Faulks <aj1331@users.noreply.github.com>
2016-01-18 04:00:50 +1100

    Improved wordcloud rendering and updating
    Updates now occur straight away if it has been more than 2000ms since
    the last update, rather than on a fixed 2000ms interval.
    
    The range of font sizes changes based on the number responses to more
    accurately reflect the relative weighting of each word.
    
    Issue with d3.layout.cloud not always loading is fixed.

--- client/plugins/word-cloud/word-cloud.directive.js
+++ client/plugins/word-cloud/word-cloud.directive.js
@@ -5,191 +5,150 @@
     .module('plugins.word-cloud')
     .directive('wordCloud', wordCloud);
       
-  wordCloud.$inject = ['$log', '$q', '$interval', '$window', 'plugins'];
+  wordCloud.$inject = [
+    '$log', 
+    '$compile', 
+    '$q', 
+    '$window', 
+    'plugins'
+  ];
     
-  function wordCloud($log, $q, $interval, $window, plugins) {
+  function wordCloud(
+      $log, 
+      $compile,
+      $q, 
+      $window, 
+      plugins
+    ) {
     $log = $log.getInstance('wordCloud');
     
     var directive = {
       link: link,
       restrict: 'EA',
       scope: {
-        words: '=',
-        wordsUpdated: '='
+        words: '='
       },
     }
     return directive;
     
-    function link(scope, ele, attrs) {
-      var loadScripts = [
-        plugins.loadScript('assets/bower_components/d3/d3.min.js', 'd3'),
-        plugins.loadScript('assets/bower_components/d3-cloud/build/d3.layout.cloud.js', 'cloud'),
-      ];
+    function link(scope, element, attrs) {      
+      plugins.loadScript('assets/bower_components/d3/d3.min.js', 'd3')
+        .then(function() {
+          return plugins.loadScript('assets/bower_components/d3-cloud/build/d3.layout.cloud.js', 'cloud');
+        })
+        .then(function() {
+          wordcloud(scope, element[0]);
+        });
+    }
     
-      $q.all(loadScripts).then(function(scripts) {
-        var d3 = scripts[0],
-            parentElement = ele[0];
-        
-        var words = [],
-            immediate = false;
-                
-        var svg = d3.select(parentElement).append('svg'),
-            g = svg.append('g');
-          
-        var width = 500,
-            height = 200;
-          
-        var fill = d3.scale.category20();
-          
-        var layout = d3.layout.cloud()
-          .padding(5)
-          .rotate(0)
-          .font('Impact')
-          .text(function(d) { return d.text })
-          .on('end', render);
-        
-        function render(words, size) {
-          $log.debug('render');
-          $log.debug('words = ', words);
-          
-          var scale = size
-            ? Math.min(
-                width / Math.abs(size[1].x - width / 2), 
-                width / Math.abs(size[0].x - width / 2), 
-                height / Math.abs(size[1].y - height / 2), 
-                height / Math.abs(size[0].y - height / 2)
-              ) / 2
-            : 1;
-        
-          var text = g.selectAll('text')
-            .data(words, function(d) { return d.text.toLowerCase(); });
+    function wordcloud(scope, parentElement) {
+      var fill = d3.scale.category20(),
+          height = Number.NaN,
+          width = Number.NaN,
+          words = [];
               
-          text.transition()
+      var svg = d3.select(parentElement).append('svg'),
+          g = svg.append('g');
+        
+      var layout = d3.layout.cloud()
+        .padding(5)
+        .rotate(0)
+        .font('Impact')
+        .text(function(d) { return d.text })
+        .on('end', render);
+        
+      
+      function render(words, size) {
+        $log.debug('render');
+        
+        var cloud = g.selectAll('text')
+          .data(words, function(d) { return d.text; });
+        
+        cloud.enter()
+          .append('text')
+            .attr('font-size', 1)
+            .attr('text-anchor', 'middle')
+            .style('fill', function(d, i) { return fill(i); })
+            .style('font-family', 'Impact')
+            .text(function(d) { return d.text; })
+            
+        cloud
+          .transition()
             .duration(1000)
             .attr('transform', function(d) {
               return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
             })
             .style('font-size', function(d) { return d.size + 'px'; })
-              
-          text.enter().append('text')
-            .attr('text-anchor', 'middle')
-            .attr('transform', function(d) {
-              return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
-            })
-            .style('font-family', 'Impact')
-            .style('font-size', '1px')
-            .style('fill', function(d, i) { return fill(i); })
-            .text(function(d) { return d.text })
-            .transition()
-              .duration(1000)
-              .style('font-size', function(d) { return d.size + 'px'; });
             
-          if (immediate) {
-            g.attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')scale(' + scale + ')');
-          }
-          else {
-            g.transition()
-              .delay(1000)
-              .duration(750)
-              .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')scale(' + scale + ')');
-          }
-        }
-        
-        function update(words) {
-          svg.style('width', '100%');
-          // A 5:2 aspect ratio seemed to work well with the space available
-          // at common screen resolutions.
-          width = parseInt(svg.style('width'), 10)
-          height = width * 2 / 5;
-          
-          var values = _.pluck(words, 'value'),
-              min = _.min(values),
-              max = _.max(values);
-          
-          var fontSize = d3.scale.linear()
-            .range([width * 0.025, width * 0.1]);
-          if (words.length)
-            fontSize.domain([min, max]);
+        cloud.exit()
+          .transition()
+            .duration(200)
+            .attr('font-size', 1)
+            .style('fill-opacity', 1e-6)
+            .remove();
             
-          $log.debug('fontSize.domain = ', fontSize.domain());
-   
-          // Don't proceed if height or width is NaN, it causes the browser
-          // to lock up during layout.
-          if (isNaN(width) || isNaN(height))
-            return;
-   
-          svg
-            .attr('width', width)
-            .attr('height', height);
-              
-          layout
-            .stop()
-            .size([width, height])
-            .fontSize(function(d) { return fontSize(d.value); })
-            .words(words)
-            .start();
-        }
+        g
+          .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+      }
+      
+      function update(words) {
+        $log.debug('update');
         
-        // Initial render
+        // A 5:2 aspect ratio seemed to work well with the space available
+        // at common screen resolutions.
+        svg.style('width', '100%');
+        width = parseInt(svg.style('width'), 10)
+        height = width * 2 / 5;
+        
+        // Don't proceed if height or width is NaN, it can cause the 
+        // browser to lock up during layout.
+        if (isNaN(width) || isNaN(height))
+          return;
+          
+        svg
+          .attr('width', width)
+          .attr('height', height);
+        
+        var values = _.pluck(words, 'value'),
+            min = _.min(values),
+            max = _.max(values),
+            sumOfUnique = _.reduce(_.unique(values), function(a, b) {
+              return a + b;
+            });
+        
+        var minFontSize = Math.max(100 - 10 * values.length, 25),
+            maxFontSize = Math.min(minFontSize + 10 * sumOfUnique, 100),
+            fontSize = d3.scale.log()
+              .range([minFontSize, maxFontSize]);
+        if (words.length)
+          fontSize.domain([min, max]);
+            
+        layout
+          .stop()
+          .size([width, height])
+          .fontSize(function(d) { return fontSize(d.value); })
+          .words(words)
+          .start();
+      }
+      var updateThrottled = _.throttle(update, 2000);
+  
+      // SVG elements use pixel sizes, so the sizes need to be recalculated when 
+      // the window is resized to remain proportional.
+      var onResize = _.debounce(function() {
         update(words);
-    
-        // SVG elements use pixel sizes, so the sizes need to be recalculated when 
-        // the window is resized to remain proportional.
-        var onResize = function() {
-          update(words);
-          scope.$apply();
-        }
-        angular.element($window).bind('resize', onResize);
-        scope.$on('$destroy', function() {
-          angular.element($window).unbind('resize', onResize);
-        });
-        
-        // Updating too quickly looks terrible. Limit updates to 
-        // 3sec intervals.
-        var updateInterval = $interval(function() {
-          if (!scope.wordsUpdated) 
-            return;
-          $log.debug('words = ', words);
-          update(words);
-          scope.wordsUpdated = false;
-        }, 3000);
-        
-        scope.$on('$destroy', function() {
-          $interval.cancel(updateInterval);
-        });
-
-        // Watch for data changes and flag updates
-        scope.$watch('wordsUpdated', function(newValue, oldValue) {
-          if (!newValue) {
-            scope.wordsUpdated = false;
-            return;
-          }
-          if(!angular.isArray(scope.words)) {
-            $log.warn('Attribute "words" should be an array');
-            scope.wordsUpdated = false;
-            return;
-          }
-          words = scope.words
-        });
-        
-        // When the words collection is first set, render the word cloud
-        // straight away without animations.
-        var wordsWatch = scope.$watch('words', function(newValue, oldValue) {
-          if(!angular.isArray(scope.words)) {
-            $log.warn('Attribute "words" should be an array');
-            scope.wordsUpdated = false;
-            return;
-          }
-          words = scope.words
-          immediate = true;
-          update(words);
-          immediate = false;
-          scope.wordsUpdated = false;
-          // Remove the watch
-          wordsWatch();
-        });
-
+        scope.$apply();
+      }, 500);
+      angular.element($window).bind('resize', onResize);
+      scope.$on('$destroy', function() {
+        angular.element($window).unbind('resize', onResize);
       });
+      
+      scope.$watch('words', function(newValue, oldValue) {
+        if(!angular.isArray(newValue))
+          return;
+        angular.copy(scope.words, words);
+        updateThrottled(words);
+      }, true);
     }
     
   }
