2fdc09e219a732a57495c731f6fc230812da6342
Nathan Sherburn <nathansherburn@users.noreply.github.com>
2016-05-06 15:21:02 +1000

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = function() {
 
-  mongoose.connect('mongodb://localhost/mars');
+  mongoose.connect('mongodb://ds015902.mlab.com:15902/heroku_gn9rm30l');
   mongoose.Promise = require('bluebird');
   
   return mongoose;
