a3b4ce174debcff0300e62782483c7bffa056de7
Nathan Sherburn <nathansherburn@users.noreply.github.com>
2016-05-09 10:47:48 +1000

    Update database.js
    How did that even happen?

--- server/config/database.js
+++ server/config/database.js
@@ -11,7 +11,7 @@ module.exports = function() {
   var mongodbUri = 'mongodb://process.env.MONGODB_USERNAME:process.env.MONGODB_PASSWORD@ds015902.mlab.com:15902/heroku_gn9rm30l';
    
   mongoose.connect(mongodbUri, options);
-  mongoose.Promise = require('bluebird';)
+  mongoose.Promise = require('bluebird');
   
   return mongoose;
 
