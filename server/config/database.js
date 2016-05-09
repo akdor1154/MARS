0548556aeeab9e3a265b1b1478a90923c32b55ab
Nathan Sherburn <nathansherburn@users.noreply.github.com>
2016-05-09 10:50:43 +1000

    Update database.js
    That's why you don't copy and paste

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 module.exports = function() {
 
-  mongoose.connect('mongodb://ds015902.mlab.com:15902/heroku_gn9rm30l');
   var options = {
     server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
     replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }
