c6c88f58660fc0294f48168df381e8b97114beb4
aj1331 <aj.faulks@gmail.com>
2016-05-16 07:17:47 +1000

    Fix #24: Use state change events to hide menu instead of `$scope.$on('$destroy', ...)` event
    The $scope '$destroy' event can happen after the next state runs it's
    `activate` function. This causes the new "3 dots menu" to be removed.
    Changing to state change events provides finer control over when the reset
    happens.

--- client/app/my-polls/shell/shell.service.js
+++ client/app/my-polls/shell/shell.service.js
@@ -5,10 +5,12 @@
     .module('app.myPolls')
     .factory('shell', shell);
       
-  shell.$inject = ['$log'];
+  shell.$inject = ['$log', '$rootScope'];
     
-  function shell($log) {
+  function shell($log, $rootScope) {
     $log = $log.getInstance('shell');
+    
+    init();
     
     var service = {
       back: {
@@ -36,6 +38,10 @@
     };
     return service;
     
+    function init() {
+      $rootScope.$on('$stateChangeSuccess', reset);
+    }
+    
     function clearSearch() {
       service.search.phrase = '';
       searchPhraseChanged();
@@ -44,6 +50,17 @@
     function hideSearch() {
       clearSearch();
       service.search.show = false;
+    }
+    
+    function reset() {
+      service.back.state = null;
+      service.back.stateParams = null;
+      service.menu.items = [];
+      service.menu.show = false;
+      service.search.callback = false;
+      service.search.phrase = '';
+      service.search.show = false;
+      service.title = '';
     }
     
     function searchPhraseChanged() {
@@ -55,34 +72,34 @@
     function setBack($scope, state, stateParams) {
       service.back.state = state;
       service.back.stateParams = stateParams;
-      $scope.$on('$destroy', function() {
-        service.back.state = null;
-        service.back.stateParams = null;
-      });
+      // $scope.$on('$destroy', function() {
+      //   service.back.state = null;
+      //   service.back.stateParams = null;
+      // });
     }
     
     function setMenu($scope, menuItems) {
       service.menu.items = menuItems;
       service.menu.show = true;
-      $scope.$on('$destroy', function() {
-        service.menu.items = [];
-        service.menu.show = false;
-      });
+      // $scope.$on('$destroy', function() {
+      //   service.menu.items = [];
+      //   service.menu.show = false;
+      // });
     }
     
     function setSearch($scope, searchCallback) {
       service.search.callback = searchCallback;
-      $scope.$on('$destroy', function() {
-        service.search.callback = false;
-        hideSearch();
-      });
+      // $scope.$on('$destroy', function() {
+      //   service.search.callback = false;
+      //   hideSearch();
+      // });
     }
     
     function setTitle($scope, title) {
       service.title = title;
-      $scope.$on('$destroy', function() {
-        service.title = '';
-      });
+      // $scope.$on('$destroy', function() {
+      //   service.title = '';
+      // });
     }
     
     function showSearch() {
