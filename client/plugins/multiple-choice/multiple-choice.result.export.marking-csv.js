f9cf3a6d78ac7156e55505ab8e625066b50ed4dd
Nathan Sherburn <nathansherburn@gmail.com>
2016-05-17 13:49:33 +1000

    Update marking csv export with merged sessions

--- client/plugins/multiple-choice/multiple-choice.result.export.marking-csv.js
+++ client/plugins/multiple-choice/multiple-choice.result.export.marking-csv.js
@@ -33,60 +33,130 @@
     
     function exportResults(results) {
       var csv = csvWriter();
-      addGroupHeader(csv, results);
-      addPollTextHeader(csv, results);
-      addCorrectAnswerHeader(csv, results);
-      createUserRows(csv, results);
+
+        /*
+          Group responses with the same group, collection, question, choices.
+          
+          Output: groupedResults = {
+            'identifier1': [{result}, {result}, ... ],
+            'identifier2': [{result}, {result}, ... ],
+            'identifier3': [{result}, {result}, ... ]
+            ...
+          }
+        */
+        var groupedResults = _.groupBy(results, function(result) {
+        var identifier = result.group
+         + result.pollCollection
+         + result.poll.data.question
+         + JSON.stringify(result.poll.data.choices);
+        return identifier;
+      });
+
+
+      /*
+        Merge the responses for each set of grouped results.
+        
+        Output: mergedGrouped = {
+          'identifier1': {
+            group: 'Week 1',
+            pollText: 'Am I really just a poll question?',
+            choices: [ {choice1}, {choice2} ... ],
+            responses: {
+              user1: { answer: 'A', time: 'Sun May 15 2016 13:43:22 GMT+1000 (AEST)' }
+              ...
+            }
+          },
+          ...
+        }
+      */      
+      var mergedGrouped = {};      
+      _.each(groupedResults, function (results, identifier) {
+        if (!mergedGrouped[identifier])
+          mergedGrouped[identifier] = {
+            group: results[0].poll.group.name,
+            pollText: results[0].poll.data.question,
+            choices: results[0].poll.data.choices
+          };
+
+        if (!mergedGrouped[identifier].responses)
+           mergedGrouped[identifier].responses = {};
+           
+        _.each(results, function (result) {
+          _.each(result.responses, function (response) {
+            var username = response.user.username;
+            var userAnswer = response.data;
+            var time =  new Date(response.time);
+            if (mergedGrouped[identifier].responses[username]) {
+              if (mergedGrouped[identifier].responses[username].time < time) {
+                mergedGrouped[identifier].responses[username] = {
+                  answer: userAnswer,
+                  time: time
+                };
+              }
+            } else {
+              mergedGrouped[identifier].responses[username] = {
+                answer: userAnswer,
+                time: time
+              };
+            }
+          });
+        });
+        
+      });
+          
+      addGroupHeader(csv, mergedGrouped);
+      addPollTextHeader(csv, mergedGrouped);
+      addCorrectAnswerHeader(csv, mergedGrouped);
+      createUserRows(csv, mergedGrouped);
       return $q.resolve(csv.blob());
     }
     
     function addGroupHeader(csv, results) {
-      var groups = _.map(results, function(result) {
-        return result.poll.group.name;
-      })
-      csv.addRow(['GROUP'].concat(groups))
+      var groupsRow = _.map(results, function(result) {
+        return result.group;
+      });
+      csv.addRow(['GROUP'].concat(groupsRow));
     }
 
     function addPollTextHeader(csv, results) {
       var text = _.map(results, function(result) {
-        return result.poll.data.question;
-      })
-      csv.addRow(['POLL TEXT'].concat(text))
+        return result.pollText;
+      });
+      csv.addRow(['POLL TEXT'].concat(text));
     }
-    
+
     function addCorrectAnswerHeader(csv, results) {
       var answers = _.map(results, function(result) {
-        var correctAnswers = _.reduce(result.poll.data.choices,
+        var correctAnswers = _.reduce(result.choices,
         function(answers, choice) {
-          var answer = (choice.correct ? choice.label : '')
-          return (answers + answer)
-        }, '')
+          var answer = (choice.correct ? choice.label : '');
+          return (answers + answer);
+        }, '');
         return correctAnswers;
-      })
-      csv.addRow(['CORRECT ANSWER(S)'].concat(answers))
+      });
+      csv.addRow(['CORRECT ANSWER(S)'].concat(answers));
     }
     
     function createUserRows (csv, results) {
       // Create an index of user responses 
-      var userResponses = {}
-      _.each(results, function (result) {
-        _.each(result.responses, function (response) {
-          var username = response.user.username
+      var userResponses = {};
+      _.each(results, function (result, identifier) {
+        _.each(result.responses, function (response, username) {
           if (!userResponses[username])
-            userResponses[username] = {}
-          userResponses[username][result._id] = response.data
-        })
-      })
-    
+            userResponses[username] = {};
+          userResponses[username][identifier] = response.answer;
+        });
+      });
+      
       // Add each student row to csv
       var usernames = _.sortBy(_.keys(userResponses),
-      function (n) {return n}) 
+      function (n) {return n;});
       _.each(usernames, function (username) {
-        var userAnswers = _.map(results, function (result) {
-          return userResponses[username][result._id] || ''
-        })
-        csv.addRow([username].concat(userAnswers))
-      })
+        var userAnswers = _.map(results, function (result, identifier) {
+          return userResponses[username][identifier] || '';
+        });
+        csv.addRow([username].concat(userAnswers));
+      });
     }
       
   }
